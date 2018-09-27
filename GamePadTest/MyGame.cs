074304aa332aff3fb698b6bfab3bca59d00c23fb
Jibaut <Guillaume.Jibaut@Konicaminolta.fr>
2018-09-27 11:00:53 +0200

    Ajoutez des fichiers projet.

---
+++ GamePadTest/MyGame.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GamePadTest
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class MyGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public GamePadState CurrentPadState { get; private set; }
+        public SpriteFont ArialFont { get; private set; }
+
+        public MyGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            ArialFont = Content.Load<SpriteFont>("Fonts/ArialFont");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            CurrentPadState = GamePad.GetState(PlayerIndex.One);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            RenderGamePadState();
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private readonly Vector2 ButtonAPos = new Vector2(2, 2);
+        private void RenderGamePadState()
+        {
+            // Bouton A
+            spriteBatch.DrawString(
+                ArialFont,
+                string.Format(""),
+                ButtonAPos,
+                Color.White
+            );
+
+
+        }
+    }
+}
